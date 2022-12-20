a8fcc22da72528a6d7574ae76b081a2ef45447b7
digitalprashant07 <digitalprashant07@outlook.com>
2022-12-20 11:20:10 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Dashboard from './Pages/Dashboard';
+import Analytics from './Pages/Analytics';
+import FileManager from './Pages/FileManager';
+import Messages from './Pages/Messages';
+import Order from './Pages/Order';
+import Saved from './Pages/Saved';
+import Setting from './Pages/Setting';
+import Users from './Pages/Users';
+import Sidebar from './Components/Sidebar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      <Sidebar>
+        <Routes>
+          <Route path='/' element={<Dashboard/>} />
+          <Route path='/users' element={<Users/>} />
+          <Route path='/dashboard' element={<Dashboard/>} />
+          <Route path='/analytics' element={<Analytics/>} />
+          <Route path='/file-manager' element={<FileManager/>} />
+          <Route path='/messages' element={<Messages/>} />
+          <Route path='/order' element={<Order/>} />
+          <Route path='/saved' element={<Saved/>} />
+          <Route path='/setting' element={<Setting/>} />
+          <Route path='*' element={<>not found</>} />
+        </Routes>
+        </Sidebar>
+      </Router>
     </div>
   );
 }
---
+++ src/Components/Sidebar.jsx
@@ -0,0 +1,147 @@
+import { motion, AnimatePresence } from 'framer-motion/dist/framer-motion'
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import { FaBars, FaHome } from 'react-icons/fa';
+import { MdDashboard } from "react-icons/md";
+import { TbBrandGoogleAnalytics } from "react-icons/tb";
+import { AiTwotoneFileExclamation } from "react-icons/ai";
+import { AiFillHeart } from "react-icons/ai";
+import { MdOutlineMessage } from "react-icons/md";
+import { BsCartCheck } from "react-icons/bs";
+import { BiCog, BiSearch } from "react-icons/bi";
+import { HiUsers } from "react-icons/hi";
+
+
+
+
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    icon: <FaHome />,
+  },
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    icon: <MdDashboard />,
+  },
+  {
+    path: "/analytics",
+    name: "Analytics",
+    icon: <TbBrandGoogleAnalytics />,
+  },
+  {
+    path: "/file-manager",
+    name: "FileManager",
+    icon: <AiTwotoneFileExclamation />,
+  },
+  {
+    path: "/messages",
+    name: "Messages",
+    icon: <MdOutlineMessage />,
+  },
+  {
+    path: "/order",
+    name: "Order",
+    icon: <BsCartCheck />,
+  },
+  {
+    path: "/saved",
+    name: "Saved",
+    icon: <AiFillHeart />,
+  },
+  {
+    path: "/setting",
+    name: "Setting",
+    icon: <BiCog />,
+  },
+  {
+    path: "/users",
+    name: "Users",
+    icon: <HiUsers />,
+  },
+];
+
+const Sidebar = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+  const inputAnimation = {
+    hidden: {
+      width: 0,
+      padding: 0,
+      opacity:0,
+      transition: {
+        duration: 0.2,
+      },
+    },
+    show: {
+      width: "140px",
+      padding: "5px 15px",
+      opacity:1,
+      transition: {
+        duration: 0.2,
+      },
+    },
+  };
+
+  const showAnimation = {
+    hidden: {
+      width: 0,
+      opacity:0,
+      transition: {
+        duration: 0.5,
+      },
+    },
+    show: {
+      width: "auto",
+      opacity:1,
+      transition: {
+        duration: 0.2,
+      },
+    },
+  };
+
+
+  return (
+    <div className="main-container">
+      <motion.div animate={{ width: isOpen ? "200px" : "35px", transition:{
+        duration:0.5,
+        type:'spring',
+        damping:12,
+      } }} className="sidebar">
+        <div className='top_section'>
+          {isOpen && <motion.h1 variants={showAnimation} initial="hidden" animate="show" exit="hidden" className='logo'>DigitalArc</motion.h1>}
+          <div className='bars'>
+            <FaBars onClick={toggle} />
+          </div>
+        </div>
+        <div className='search'>
+          <div className='search_icons'>
+            <BiSearch />
+          </div>
+          <AnimatePresence>
+            {isOpen && <motion.input initial="hidden" animate="show" exit="hidden" variants={inputAnimation} placeholder='search...' />}
+          </AnimatePresence>
+        </div>
+        <section className='routes'>
+          {routes.map((route) => (
+            <NavLink activeClassName="active" to={route.path} key={route.name} className='link'>
+              <div className='icon'>
+                {route.icon}
+              </div>
+              <AnimatePresence>
+                {isOpen && <motion.div variants={showAnimation} initial="hidden" animate="show" exit="hidden" className='link_text'>{route.name}</motion.div>}
+              </AnimatePresence>
+            </NavLink>
+          ))}
+        </section>
+      </motion.div>
+      <main>
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default Sidebar;
